43aaa6029d4885eec18d0e1609b09ac85ab0250f
6kbgdb7k85-stack <6kbgdb7k86@privaterelay.appleid.com>
2026-07-18 11:13:34 -0400

    serach feature complete

--- course-5-summative-lab/src/components/Shop.jsx
+++ course-5-summative-lab/src/components/Shop.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import useFetch from "../common/utils/useFetch";
 import ProductCard from "./ProductCard";
+import FormField from "../common/components/FormField";
 
 export default function Shop() {
   const { loading: shopLoading, response: products } = useFetch(
     "http://localhost:6001/products",
   );
+
+  const [search,setSearch]=useState('')
+
+  const filteredProducts = products?.filter(product=>product.name.toLowerCase().includes(search.toLowerCase()));
 
   return (
     <>
@@ -14,7 +19,8 @@ export default function Shop() {
         <h4>Loading...</h4>
       ) : (
         <>
-          {products?.map((product) => (
+        <input type="text" placeholder="Search by product name..." onChange={(e)=>setSearch(e.target.value)} value={search}/>
+          {filteredProducts?.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
         </>
